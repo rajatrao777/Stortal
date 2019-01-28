276a81b3894e0b743271d0a430e5da51739cf7f0
rajatrao777 <rajatrao777@gmail.com>
2019-01-28 10:32:20 +0530

    Updates

---
+++ precache-manifest.c32b4753ab3688a8581d7f70e0c792b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce13f8b21d078f6e2179",
+    "url": "/Stortal/static/js/runtime~main.ce13f8b2.js"
+  },
+  {
+    "revision": "6c22556448d24efb82e3",
+    "url": "/Stortal/static/js/main.6c225564.chunk.js"
+  },
+  {
+    "revision": "d12b40564aeca6ffe03f",
+    "url": "/Stortal/static/js/1.d12b4056.chunk.js"
+  },
+  {
+    "revision": "6bdc2ca806838f7a74b25d48dea5d552",
+    "url": "/Stortal/index.html"
+  }
+];
